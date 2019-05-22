b59189e5b1459044f1eea976efe52665753622dc
ricardo.poncio <ricardo.poncio@LUME064-2.lume.intra>
2019-05-22 16:20:27 -0300

    Correção classe startup.

--- src/br/com/lume/common/config/OdontoInitializer.java
+++ src/br/com/lume/common/config/OdontoInitializer.java
@@ -6,6 +6,7 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.servlet.ServletContextEvent;
 
+import br.com.lume.configuracao.Configuracao;
 import br.com.lume.configuracao.Configurar;
 
 @Singleton
@@ -14,7 +15,7 @@ public class OdontoInitializer {
     
     @PostConstruct
     public void init() {
-        Configurar.getInstance().getConfiguracao().setAmbiente("DEV");
+        Configurar.getInstance().getConfiguracao().setAmbiente(Configuracao.AMBIENTES.DEV);
     }
 
     @PreDestroy
