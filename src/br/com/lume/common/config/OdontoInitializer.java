de8aa88b617e265a7d8a607573323abe8e3474bc
ricardo.poncio <ricardo.poncio@LUME064-2.lume.intra>
2019-05-22 16:18:52 -0300

    Alteração de telas e classe de startup.

---
+++ src/br/com/lume/common/config/OdontoInitializer.java
@@ -0,0 +1,25 @@
+package br.com.lume.common.config;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.servlet.ServletContextEvent;
+
+import br.com.lume.configuracao.Configurar;
+
+@Singleton
+@Startup
+public class OdontoInitializer {
+    
+    @PostConstruct
+    public void init() {
+        Configurar.getInstance().getConfiguracao().setAmbiente("DEV");
+    }
+
+    @PreDestroy
+    public void destroy() {
+
+    }
+
+}
