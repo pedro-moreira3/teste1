655dd68e838334eefdd19702d91442adc96dd9ee
eduardo.tremarin <eduardo.tremarin@LUME113.lume.intra>
2019-07-23 09:58:45 -0300

    Implementação tela relatorioProcedimento

--- src/br/com/lume/odonto/managed/RelatorioProcedimentoMB.java
+++ src/br/com/lume/odonto/managed/RelatorioProcedimentoMB.java
@@ -1,7 +1,7 @@
 package br.com.lume.odonto.managed;
 
-import java.math.BigDecimal;
 import java.text.Normalizer;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -10,140 +10,144 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.event.ActionEvent;
 
 import org.apache.log4j.Logger;
 
 import br.com.lume.common.managed.LumeManagedBean;
 import br.com.lume.common.util.Mensagens;
 import br.com.lume.common.util.UtilsFrontEnd;
+import br.com.lume.convenio.ConvenioSingleton;
+import br.com.lume.odonto.entity.Convenio;
 import br.com.lume.odonto.entity.Paciente;
+import br.com.lume.odonto.entity.PlanoTratamento;
 import br.com.lume.odonto.entity.PlanoTratamentoProcedimento;
 import br.com.lume.odonto.entity.Profissional;
-import br.com.lume.odonto.entity.RelatorioProcedimento;
-import br.com.lume.odonto.util.OdontoMensagens;
 import br.com.lume.paciente.PacienteSingleton;
+import br.com.lume.planoTratamento.PlanoTratamentoSingleton;
 import br.com.lume.planoTratamentoProcedimento.PlanoTratamentoProcedimentoSingleton;
 import br.com.lume.profissional.ProfissionalSingleton;
-import br.com.lume.relatorioProcedimento.RelatorioProcedimentoSingleton;
 
 @ManagedBean
 @ViewScoped
-public class RelatorioProcedimentoMB extends LumeManagedBean<RelatorioProcedimento> {
+public class RelatorioProcedimentoMB extends LumeManagedBean<PlanoTratamentoProcedimento> {
 
     private static final long serialVersionUID = 1L;
 
     private Logger log = Logger.getLogger(RelatorioProcedimentoMB.class);
+    
+    private List<PlanoTratamentoProcedimento> listaProcedimentos;
 
-    private Profissional profissional;
-
-    private List<PlanoTratamentoProcedimento> relatorioProcedimentos = new ArrayList<>();
-
-    private Date inicio, fim;
-
-    private BigDecimal totalValor;
-
-
+    
+    // ATRIBUTOS USADOS COMO FILTRO PARA PESQUISA DOS PROCEDIMENTOS
+    private Profissional filtroPorProfissional;
+    private PlanoTratamento filtroPorPlanoTratamento;
+    private Paciente filtroPorPaciente;
+    private Date dataInicio, dataFim;
+    private Convenio filtroPorConvenio;
 
     public RelatorioProcedimentoMB() {
-        super(RelatorioProcedimentoSingleton.getInstance().getBo());
- 
-        this.setClazz(RelatorioProcedimento.class);
-        this.carregarDatasIniciais();
-        this.filtra();
+        super(PlanoTratamentoProcedimentoSingleton.getInstance().getBo());
+        this.setClazz(PlanoTratamentoProcedimento.class);
     }
-
-    private void carregarDatasIniciais() {
-        Calendar c = Calendar.getInstance();
-        this.fim = c.getTime();
-        c.set(Calendar.DAY_OF_MONTH, 1);
-        this.inicio = c.getTime();
-    }
-
-    public void filtra() {
-        this.totalValor = BigDecimal.ZERO;
-        // totalValorServico = BigDecimal.ZERO;
-        if (this.inicio != null && this.fim != null) {
-            if (this.inicio.getTime() <= this.fim.getTime()) {
-                Calendar c = Calendar.getInstance();
-                c.setTime(this.fim);
-                c.add(Calendar.DAY_OF_MONTH, 1);
-                this.fim = c.getTime();
-            } else {
-                this.addError(OdontoMensagens.getMensagem("afastamento.dtFim.menor.dtInicio"), "");
+    
+    public void popularLista() {
+        try{
+            
+            if(getDataInicio() == null && getDataFim() == null) {
+                
+                Calendar calendario = Calendar.getInstance();
+                this.setDataFim(calendario.getTime());
+                calendario.add(Calendar.DAY_OF_MONTH, -7);
+                this.setDataInicio(calendario.getTime());
+                
             }
-            this.relatorioProcedimentos = PlanoTratamentoProcedimentoSingleton.getInstance().getBo().listRelatorioProcedimento(this.profissional, this.inicio, this.fim, UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
-            for (PlanoTratamentoProcedimento planoTratamentoProcedimento : this.relatorioProcedimentos) {
-                this.totalValor = this.totalValor.add(planoTratamentoProcedimento.getValorDesconto());
-            }
+            
+            this.listaProcedimentos = PlanoTratamentoSingleton.getInstance().getBo().listByDataAndProfissionalAndPaciente(this.dataInicio, this.dataFim, this.filtroPorPaciente, 
+                    this.filtroPorProfissional, this.filtroPorPlanoTratamento, this.filtroPorConvenio, UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
+            
+        }catch(Exception e){
+            this.addError(Mensagens.getMensagem(Mensagens.ERRO_AO_BUSCAR_REGISTROS), "");
+            log.error(Mensagens.ERRO_AO_BUSCAR_REGISTROS, e);
         }
     }
-
-    @Override
-    public void actionNew(ActionEvent arg0) {
-        this.profissional = null;
-        super.actionNew(arg0);
-    }
-
-    public List<Paciente> geraSugestoesPaciente(String query) {
-        return PacienteSingleton.getInstance().getBo().listSugestoesComplete(query, UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
-    }
-
-    public List<Profissional> geraSugestoesProfissional(String query) {
-        List<Profissional> sugestoes = new ArrayList<>();
-        List<Profissional> profissionais = new ArrayList<>();
-        try {
-            profissionais = ProfissionalSingleton.getInstance().getBo().listDentistasByEmpresa(UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
-            for (Profissional p : profissionais) {
-                if (Normalizer.normalize(p.getDadosBasico().getNome().toLowerCase(), Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").toLowerCase().contains(
-                        Normalizer.normalize(query, Normalizer.Form.NFD).toLowerCase())) {
-                    sugestoes.add(p);
-                }
-            }
-            Collections.sort(sugestoes);
-        } catch (Exception e) {
-            this.log.error(Mensagens.ERRO_AO_BUSCAR_REGISTROS);
+    
+    public String formatarData(Date data) {
+        if(data != null) {
+            return new SimpleDateFormat("dd/MM/yyyy").format(data);
         }
-        return sugestoes;
+        return "";
+    }
+    
+    public List<Paciente> sugestoesPacientes(String query) {
+        return PacienteSingleton.getInstance().getBo().listSugestoesComplete(query,UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
+    }
+    
+    public List<Profissional> sugestoesProfissionais(String query) {
+        return ProfissionalSingleton.getInstance().getBo().listSugestoesComplete(query,UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
+    }
+    
+    public List<Profissional> sugestoesProfissionalUltAlteracao(String query) {
+        return ProfissionalSingleton.getInstance().getBo().listSugestoesComplete(query,UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
+    }
+    
+    public List<Convenio> sugestoesConvenios(String query) {
+        return ConvenioSingleton.getInstance().getBo().listSugestoesComplete(query,UtilsFrontEnd.getProfissionalLogado().getIdEmpresa());
     }
 
-    public Profissional getProfissional() {
-        return this.profissional;
+    public Profissional getFiltroPorProfissional() {
+        return filtroPorProfissional;
     }
 
-    public void setProfissional(Profissional profissional) {
-        this.profissional = profissional;
+    public void setFiltroPorProfissional(Profissional filtroPorProfissional) {
+        this.filtroPorProfissional = filtroPorProfissional;
     }
 
-    public Date getInicio() {
-        return this.inicio;
+    public Paciente getFiltroPorPaciente() {
+        return filtroPorPaciente;
     }
 
-    public void setInicio(Date inicio) {
-        this.inicio = inicio;
+    public void setFiltroPorPaciente(Paciente filtroPorPaciente) {
+        this.filtroPorPaciente = filtroPorPaciente;
     }
 
-    public Date getFim() {
-        return this.fim;
+    public Date getDataInicio() {
+        return dataInicio;
     }
 
-    public void setFim(Date fim) {
-        this.fim = fim;
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
     }
 
-    public BigDecimal getTotalValor() {
-        return this.totalValor;
+    public Date getDataFim() {
+        return dataFim;
     }
 
-    public void setTotalValor(BigDecimal totalValor) {
-        this.totalValor = totalValor;
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
     }
 
-    public List<PlanoTratamentoProcedimento> getRelatorioProcedimentos() {
-        return this.relatorioProcedimentos;
+    public Convenio getFiltroPorConvenio() {
+        return filtroPorConvenio;
     }
 
-    public void setRelatorioProcedimentos(List<PlanoTratamentoProcedimento> relatorioProcedimentos) {
-        this.relatorioProcedimentos = relatorioProcedimentos;
+    public void setFiltroPorConvenio(Convenio filtroPorConvenio) {
+        this.filtroPorConvenio = filtroPorConvenio;
     }
+
+    public List<PlanoTratamentoProcedimento> getListaProcedimentos() {
+        return listaProcedimentos;
+    }
+
+    public void setListaProcedimentos(List<PlanoTratamentoProcedimento> listaProcedimentos) {
+        this.listaProcedimentos = listaProcedimentos;
+    }
+
+    public PlanoTratamento getFiltroPorPlanoTratamento() {
+        return filtroPorPlanoTratamento;
+    }
+
+    public void setFiltroPorPlanoTratamento(PlanoTratamento filtroPorPlanoTratamento) {
+        this.filtroPorPlanoTratamento = filtroPorPlanoTratamento;
+    }
+
 }
