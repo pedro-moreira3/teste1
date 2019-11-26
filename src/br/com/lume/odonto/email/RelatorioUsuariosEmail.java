65ca64a72974003b7370c98ed43878e07f6012b8
ricardo.poncio <ricardo.poncio@LUME064-2.lume.intra>
2019-11-26 15:45:34 -0200

    Correção no envio de relatorio gerencial.

--- src/br/com/lume/odonto/email/RelatorioUsuariosEmail.java
+++ src/br/com/lume/odonto/email/RelatorioUsuariosEmail.java
@@ -26,10 +26,11 @@ public class RelatorioUsuariosEmail implements Job {
             Calendar cal = Calendar.getInstance();
 
             if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
-                String emails = "joao.serra@lumetec.com.br;ariel.pires@lumetec.com.br;ricardo.poncio@lumetec.com.br;rogerio.nagata@lumetec.com.br;alvaro@consultoriolegal.com.br";
+                //joao.serra@lumetec.com.br;ariel.pires@lumetec.com.br;rogerio.nagata@lumetec.com.br;alvaro@consultoriolegal.com.br;ricardo.poncio@lumetec.com.br
+                String emails = "ricardo.poncio@lumetec.com.br";
 
                 EnviaEmail.enviaEmailOffLine("no-reply@intelidente.com", emails, "Intelidente - Resumo semanal de usuários ", RelatorioGerencialSingleton.getInstance().getBo().gerarRelatorioUsuario(),
-                        Mensagens.getMensagemOffLine("email.smtpHost.homolog"));
+                        Mensagens.getMensagemOffLine("email.smtpHost.prod"));
             }
         } catch (Exception e) {
             e.printStackTrace();
