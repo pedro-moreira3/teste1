06cf6c255876ce35a38864589e010290c8f07f78
ricardo.poncio <ricardo.poncio@LUME064-2.lume.intra>
2020-03-16 16:31:07 -0300

    Criação de querys, telas e MBs para ajuste de contas.

---
+++ src/br/com/lume/odonto/managed/AjusteContasMB.java
@@ -0,0 +1,139 @@
+package br.com.lume.odonto.managed;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.component.datatable.DataTable;
+
+import br.com.lume.ajustes.AjusteContasSingleton;
+import br.com.lume.common.log.LogIntelidenteSingleton;
+import br.com.lume.common.managed.LumeManagedBean;
+import br.com.lume.common.util.Mensagens;
+import br.com.lume.common.util.UtilsFrontEnd;
+import br.com.lume.odonto.entity.AjusteContas;
+
+@ManagedBean
+@ViewScoped
+public class AjusteContasMB extends LumeManagedBean<AjusteContas> {
+
+    private static final long serialVersionUID = 1L;
+
+    private Date dataInicio, dataFim;
+    private String filtroPeriodo;
+
+    private DataTable tabelaAjustes;
+
+    public AjusteContasMB() {
+        super(AjusteContasSingleton.getInstance().getBo());
+        this.setClazz(AjusteContas.class);
+        pesquisar();
+    }
+
+    public void pesquisar() {
+        try {
+            setEntityList(AjusteContasSingleton.getInstance().getBo().findByEmpresaProprietaria(UtilsFrontEnd.getEmpresaLogada()));
+        } catch (Exception e) {
+            LogIntelidenteSingleton.getInstance().makeLog(e);
+            addError("Erro", Mensagens.getMensagem(Mensagens.ERRO_AO_BUSCAR_REGISTROS));
+        }
+    }
+
+    public void actionTrocaDatasCriacao() {
+        try {
+            this.dataInicio = getDataInicio(filtroPeriodo);
+            this.dataFim = getDataFim(filtroPeriodo);
+        } catch (Exception e) {
+            LogIntelidenteSingleton.getInstance().makeLog(e);
+            this.addError(Mensagens.getMensagem(Mensagens.ERRO_AO_BUSCAR_REGISTROS), "");
+        }
+    }
+
+    public Date getDataFim(String filtro) {
+        Date dataFim = null;
+        try {
+            Calendar c = Calendar.getInstance();
+            if ("O".equals(filtro)) {
+                c.add(Calendar.DAY_OF_MONTH, -1);
+                dataFim = c.getTime();
+            } else if (filtro == null) {
+                dataFim = null;
+            } else {
+                dataFim = c.getTime();
+            }
+            return dataFim;
+        } catch (Exception e) {
+            LogIntelidenteSingleton.getInstance().makeLog(e);
+            this.addError(Mensagens.getMensagem(Mensagens.ERRO_AO_BUSCAR_REGISTROS), "");
+            return null;
+        }
+    }
+
+    public Date getDataInicio(String filtro) {
+        Date dataInicio = null;
+        try {
+            Calendar c = Calendar.getInstance();
+            if ("O".equals(filtro)) {
+                c.add(Calendar.DAY_OF_MONTH, -1);
+                dataInicio = c.getTime();
+            } else if ("H".equals(filtro)) { //Hoje                
+                dataInicio = c.getTime();
+            } else if ("S".equals(filtro)) { //Últimos 7 dias              
+                c.add(Calendar.DAY_OF_MONTH, -7);
+                dataInicio = c.getTime();
+            } else if ("Q".equals(filtro)) { //Últimos 15 dias              
+                c.add(Calendar.DAY_OF_MONTH, -15);
+                dataInicio = c.getTime();
+            } else if ("T".equals(filtro)) { //Últimos 30 dias                
+                c.add(Calendar.DAY_OF_MONTH, -30);
+                dataInicio = c.getTime();
+            } else if ("M".equals(filtro)) { //Mês Atual              
+                c.set(Calendar.DAY_OF_MONTH, 1);
+                dataInicio = c.getTime();
+            } else if ("I".equals(filtro)) { //Mês Atual
+                c.add(Calendar.MONTH, -6);
+                dataInicio = c.getTime();
+            }
+            return dataInicio;
+        } catch (Exception e) {
+            LogIntelidenteSingleton.getInstance().makeLog(e);
+            this.addError(Mensagens.getMensagem(Mensagens.ERRO_AO_BUSCAR_REGISTROS), "");
+            return null;
+        }
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getFiltroPeriodo() {
+        return filtroPeriodo;
+    }
+
+    public void setFiltroPeriodo(String filtroPeriodo) {
+        this.filtroPeriodo = filtroPeriodo;
+    }
+
+    public DataTable getTabelaAjustes() {
+        return tabelaAjustes;
+    }
+
+    public void setTabelaAjustes(DataTable tabelaAjustes) {
+        this.tabelaAjustes = tabelaAjustes;
+    }
+
+}
