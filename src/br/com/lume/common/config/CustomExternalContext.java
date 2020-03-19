6e9c7ad50f05dde225d94def571591717436461a
ariel <ariel.pires@lumetec.com.br>
2020-03-19 10:02:13 -0300

    teste socket

--- src/br/com/lume/common/config/CustomExternalContext.java
+++ src/br/com/lume/common/config/CustomExternalContext.java
@@ -18,8 +18,8 @@ public class CustomExternalContext extends ExternalContextWrapper {
        
         if(encodeWebsocketURL.contains("ltdeapp07")) {
             System.out.println("a: " + encodeWebsocketURL);     
-          //  return encodeWebsocketURL.replace("ltdeapp07:8080", "dev-intelidente.lumetec.com.br");
-            return encodeWebsocketURL.replace("ltdeapp07", "localhost");
+            return encodeWebsocketURL.replace("ltdeapp07:8080", "dev-intelidente.lumetec.com.br");
+        //    return encodeWebsocketURL.replace("ltdeapp07", "localhost");
             //8443
         }else {
             System.out.println("b: " + encodeWebsocketURL);
