95d4e6160d4ccf22e4294d287a39ed7a696d9dc9
eduardo.tremarin <eduardo.tremarin@LUME152.lume.intra>
2020-03-26 12:05:15 -0300

    Implementação statusFatura

--- src/br/com/lume/odonto/managed/LancamentoContabilMB.java
+++ src/br/com/lume/odonto/managed/LancamentoContabilMB.java
@@ -27,6 +27,7 @@ import br.com.lume.common.util.Status;
 import br.com.lume.common.util.Utils;
 import br.com.lume.common.util.UtilsFrontEnd;
 import br.com.lume.convenio.ConvenioSingleton;
+import br.com.lume.faturamento.FaturaSingleton;
 import br.com.lume.fornecedor.FornecedorSingleton;
 import br.com.lume.lancamento.LancamentoSingleton;
 import br.com.lume.lancamentoContabil.LancamentoContabilSingleton;
@@ -93,8 +94,8 @@ public class LancamentoContabilMB extends LumeManagedBean<LancamentoContabil> {
         try {
             Calendar c = Calendar.getInstance();
             fim = new Date();
-           // c.add(Calendar.MONTH, 3);
-           // fim = c.getTime();
+            // c.add(Calendar.MONTH, 3);
+            // fim = c.getTime();
             c.set(Calendar.DAY_OF_MONTH, 1);
             inicio = c.getTime();
             this.geraLista();
@@ -223,10 +224,11 @@ public class LancamentoContabilMB extends LumeManagedBean<LancamentoContabil> {
 
     public void actionValidarLancamento(Lancamento l) {
         try {
-           LancamentoContabilSingleton.getInstance().validaEConfereLancamentos(l,UtilsFrontEnd.getProfissionalLogado());    
+            LancamentoContabilSingleton.getInstance().validaEConfereLancamentos(l, UtilsFrontEnd.getProfissionalLogado());
+            FaturaSingleton.getInstance().atualizarStatusFatura(l.getFatura());
             this.carregarLancamentosValidar();
             this.addInfo(Mensagens.getMensagem(Mensagens.REGISTRO_SALVO_COM_SUCESSO), "");
-        } catch (Exception e) {         
+        } catch (Exception e) {
 
             this.addError(Mensagens.getMensagem(Mensagens.ERRO_AO_SALVAR_REGISTRO), "");
             log.error(Mensagens.ERRO_AO_SALVAR_REGISTRO, e);
