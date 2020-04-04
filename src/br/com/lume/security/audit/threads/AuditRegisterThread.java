05fac4b6c076a8b2e48ec752cfea3327d3f3a995
ricardo.poncio <ricardo.poncio@LUME064-2.lume.intra>
2020-04-04 17:21:33 -0300

    Alterações no processo de Auditoria. Alterado lugar de processamento das informações. O listener retira todas as informações do contexto em um tempo de aproximadamente 5ms. Todas as informações são salvas em um objeto específico e colocado em uma fila. A fila inicia com o sistema e verifica continuamente itens na lista para salvar em banco.

---
+++ src/br/com/lume/security/audit/threads/AuditRegisterThread.java
@@ -0,0 +1,56 @@
+package br.com.lume.security.audit.threads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.lume.audit.AuditSingleton;
+import br.com.lume.common.log.LogIntelidenteSingleton;
+import br.com.lume.security.audit.object.AuditInfo;
+
+public class AuditRegisterThread extends Thread {
+
+    private static List<AuditInfo> infos = new ArrayList<>();
+
+    public AuditRegisterThread() {
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        while (true) {
+            try {
+                // Registro os dados em banco
+                List<AuditInfo> audits2Process = new ArrayList<>(infos);
+                if (audits2Process != null && !audits2Process.isEmpty()) {
+                    List<AuditInfo> audits2RemoveFromQueue = new ArrayList<>();
+                    for (AuditInfo auditInfo : audits2Process) {
+                        AuditSingleton.getInstance().createAuditReg(auditInfo.getMethods(), auditInfo.getTimeExecution(),
+                                auditInfo.getPhaseId().getName() + " - " + auditInfo.getPhaseId().getOrdinal(), auditInfo.getTela(), auditInfo.getHost(), auditInfo.getBrowser(),
+                                auditInfo.getBrowser(), auditInfo.getProfissional());
+                        audits2RemoveFromQueue.add(auditInfo);
+                    }
+                    if (audits2RemoveFromQueue != null)
+                        for (AuditInfo audit : audits2RemoveFromQueue)
+                            removeInfoFromQueue(audit);
+                }
+            } catch (Exception e) {
+                LogIntelidenteSingleton.getInstance().makeLog(e);
+            }
+        }
+    }
+
+    private static void removeInfoFromQueue(AuditInfo info) {
+        if (infos != null && infos.indexOf(info) >= 0)
+            infos.remove(info);
+    }
+
+    public static void addInfo2Queue(AuditInfo info) {
+        if (info == null)
+            return;
+        if (infos == null)
+            infos = new ArrayList<>();
+        infos.add(info);
+    }
+
+}
