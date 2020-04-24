090b60ecf9869d9e64e75b1659543bd91ca09b95
ariel <ariel.pires@lumetec.com.br>
2020-04-24 08:38:24 -0300

    Cadastro de formas de pagamento

---
+++ sql/migracao/9 - forma de pagamento.sql
@@ -0,0 +1,17 @@
+update SEG_OBJETO set OBJ_STR_DES = 'Cadastro de Formas de Pagamento' where OBJ_STR_DES = 'Cadastro de Tarifas';
+
+alter table TARIFA add column periodicidade integer;
+alter table TARIFA add column receber_automaticamente varchar(1) default 'S';
+alter table TARIFA add column conta varchar(100);
+
+alter table TARIFA alter column bandeira drop not null;
+alter table TARIFA alter column taxa drop not null;
+alter table TARIFA alter column tarifa drop not null;
+alter table TARIFA alter column banco drop not null;
+alter table TARIFA alter column agencia drop not null;
+
+insert into tarifa (id_empresa,produto,prazo,parcela_minima,parcela_maxima,receber_automaticamente,status,tipo,excluido)
+select emp_int_cod,'Dinheiro',0,1,1,'S','A','OU','N' from seg_empresa;
+
+insert into tarifa (id_empresa,produto,prazo,parcela_minima,parcela_maxima,receber_automaticamente,status,tipo,excluido,periodicidade)
+select emp_int_cod,'Cheque',0,1,36,'S','A','OU','N',30 from seg_empresa
--- src/br/com/lume/odonto/managed/CadastroWebMB.java
+++ src/br/com/lume/odonto/managed/CadastroWebMB.java
@@ -86,6 +86,7 @@ import br.com.lume.security.UsuarioSingleton;
 // import br.com.lume.security.bo.UsuarioBO;
 import br.com.lume.security.entity.Empresa;
 import br.com.lume.security.entity.Usuario;
+import br.com.lume.tarifa.TarifaSingleton;
 
 @ManagedBean
 @ViewScoped
@@ -187,7 +188,7 @@ public class CadastroWebMB extends LumeManagedBean<Empresa> {
             this.getEntity().setValidarGeraReciboValorZerado("N");
             EmpresaSingleton.getInstance().getBo().persist(this.getEntity());
             LocalSingleton.getInstance().createLocaisDefault(EmpresaSingleton.getInstance().getBo().find(this.getEntity()).getEmpIntCod());
-            
+            TarifaSingleton.getInstance().createTarifasDefault(EmpresaSingleton.getInstance().getBo().find(this.getEntity()).getEmpIntCod());
             
             cadastrarDadosTemplate(getEntity());
             this.actionPersistFilial(null);
