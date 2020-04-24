398bafe1d56efa493a4d93f5b5a39433431bffa0
ricardo.poncio <ricardo.poncio@LUME064-2.lume.intra>
2020-04-24 13:23:01 -0300

    Criação de padrão para resgatar constantes de tooltip. Criado padrão de nomenclatura para constante de tooltip e um método na classe de padrão para resgatar as constantes de acordo com o padrão.

--- src/br/com/lume/common/managed/PadroesTelaMB.java
+++ src/br/com/lume/common/managed/PadroesTelaMB.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import br.com.lume.common.util.Mensagens;
 import br.com.lume.common.util.UtilsPadraoRelatorio.PeriodoBusca;
 import br.com.lume.odonto.entity.Paciente;
 import br.com.lume.paciente.PacienteSingleton;
@@ -25,6 +26,10 @@ public class PadroesTelaMB extends LumeManagedBean<Paciente> {
 
     public List<PeriodoBusca> getPeriodosDataBusca() {
         return PeriodoBusca.listAllValues();
+    }
+
+    public String getTooltipValue(String tela, String campo) {
+        return Mensagens.getMensagem("tooltip." + tela + "." + campo);
     }
 
 }
