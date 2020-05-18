e898a68c338cc77587e09c042ea35771d03b2768
ricvi <ricvi@192.168.104.38>
2020-05-18 14:47:47 -0300

    Correção de conferência ao refazer cálculos da negociação.

--- src/br/com/lume/odonto/managed/FaturaPagtoMB.java
+++ src/br/com/lume/odonto/managed/FaturaPagtoMB.java
@@ -1036,7 +1036,7 @@ public class FaturaPagtoMB extends LumeManagedBean<Fatura> {
         negociacaoValorTotal = getEntity().getDadosTabelaRepasseTotalFatura();
         BigDecimal valorDeDesconto = (negociacaoValorDesconto == null ? BigDecimal.ZERO : negociacaoValorDesconto);
         DescontoOrcamento descontoCadQtdeParcelas = descontosDisponiveis.get(negociacaoQuantidadeParcelas);
-        if ((descontoCadQtdeParcelas != null && valorDeDesconto.compareTo(BigDecimal.ZERO) > 0) || (valorDeDesconto.compareTo(descontoCadQtdeParcelas.getDesconto()) > 0)) {
+        if ((descontoCadQtdeParcelas == null && valorDeDesconto.compareTo(BigDecimal.ZERO) > 0) || (descontoCadQtdeParcelas != null && valorDeDesconto.compareTo(descontoCadQtdeParcelas.getDesconto()) > 0)) {
             negociacaoValorDesconto = null;
             this.addError("Erro!", "Desconto dado maior que o máximo permitido.");
             return;
