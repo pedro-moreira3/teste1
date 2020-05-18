6d9da7d234f30e09efc6976944805afbf908ee3e
Ariel José Pires Junior <ariel.pires@lumetec.com.br>
2020-05-18 10:01:29 -0300

    Update PlanoTratamentoMB.java

--- src/br/com/lume/odonto/managed/PlanoTratamentoMB.java
+++ src/br/com/lume/odonto/managed/PlanoTratamentoMB.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import javax.persistence.Query;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
