4f08f2e7576a5caff44826c381dfffbfd05bc3fb
ariel <ariel.pires@lumetec.com.br>
2020-06-02 14:40:48 -0300

    correçoes
    	OK - cadastro de formas de pagamento: cartao de credito prazo 30 dias,
    debito 2, outros 0
    	OK - incluir checbox  para incluir observacao de cobrança na impressão
    	OK - colocar opcao para nao mostrar procedimentos nao inclusos
    	OK - Orçamento: tirar a frase: Anualmente os valores são reajustados.

---
+++ sql/versionados/1.sql
@@ -0,0 +1,27 @@
+CREATE TABLE CONFIGURACOES (
+	VERSAO NUMERIC(25),
+	DATA_VERSAO TIMESTAMP(10)	
+);
+
+insert into SEG_OBJETO (obj_str_des,obj_cha_sts,obj_str_caminho,sis_int_cod,obj_int_ordem,obj_cha_tipo,obj_str_icon)
+values ('Clínicas','A','parceiroClinicas.jsf',123,1,'L','fa fa-users-md');
+
+insert into seg_perfil (sis_int_cod,per_str_des,per_cha_sts) values (123,'Parceiro','A')
+
+--INSERINDO O USUARIO DO FARUK COMO PARCEIRO
+INSERT INTO seg_perusuario (PER_INT_COD,USU_INT_COD) 
+	SELECT
+	(
+		SELECT PER_INT_COD FROM seg_perfil WHERE per_str_des = 'Parceiro'
+	)
+	,1036;
+	
+insert into OBJETO_PROFISSIONAL (obj_int_cod,id_profissional) 
+	SELECT (
+		select obj_int_cod from SEG_OBJETO where obj_str_caminho = 'parceiroClinicas.jsf'
+	),
+	994;
+
+--ULTIMO SQL DO SCRIPT SEMPRE DEVE SER NA VERSAO DA CONFIGURAÇÃO, COM O MESMO NÚMERO DO ARQUIVO.
+
+INSERT INTO CONFIGURACOES (VERSAO,DATA_VERSAO) VALUES (1,NOW())
