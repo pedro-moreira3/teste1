4ed83f762d7856cd912878552cc522307f5f9568
ariel <ariel.pires@lumetec.com.br>
2021-03-03 11:11:30 -0300

    envio de mensagens

--- src/br/com/lume/odonto/queue/EnvioMensagensConfirmacao.java
+++ src/br/com/lume/odonto/queue/EnvioMensagensConfirmacao.java
@@ -1,10 +1,18 @@
 package br.com.lume.odonto.queue;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.TimerTask;
-import br.com.lume.execucaoTimer.interfaces.Executor;
-import br.com.lume.security.EmpresaSingleton;
 
+import br.com.lume.agendamento.AgendamentoSingleton;
+import br.com.lume.execucaoTimer.interfaces.Executor;
+import br.com.lume.integracao.DetailPacoteConfirmacaoAutoSingleton;
+import br.com.lume.integracao.HistoricoMensagemIntegracaoSingleton;
+import br.com.lume.odonto.entity.Agendamento;
+import br.com.lume.odonto.entity.DetailPacoteConfirmacaoAuto;
+import br.com.lume.odonto.entity.Paciente;
+import br.com.lume.security.EmpresaSingleton;
+import br.com.lume.security.entity.Empresa;
 
 public class EnvioMensagensConfirmacao extends TimerTask implements Serializable, Executor {
 
@@ -17,11 +25,24 @@ public class EnvioMensagensConfirmacao extends TimerTask implements Serializable
     @Override
     public void run() {
         try {
-    //        EmpresaSingleton.getInstance().getBo().listEmpresasComPacotesMensagens();
-        //
+            List<Agendamento> agendamentos = AgendamentoSingleton.getInstance().getBo().listAgendamentosParaEnvioConfirmacaoAutomatica();
+            for (Agendamento agendamento : agendamentos) {                
+                Paciente paciente = agendamento.getPaciente();
+                Empresa empresa = EmpresaSingleton.getInstance().getBo().find(paciente.getIdEmpresa());
+                boolean retornoEnvio  = HistoricoMensagemIntegracaoSingleton.getInstance().enviaMensagemAutomaticaParaCliente(paciente, agendamento, empresa);
+                if(retornoEnvio) {
+                    //TODO transformar em log para salvar em tb to banco
+                    System.out.println("MENSAGEM CONFIRMACAO: Mensagem enviada com sucesso para o agendamento id: " + agendamento.getId());
+                }else {
+                    //TODO transformar em log para salvar em tb to banco
+                    System.out.println("MENSAGEM CONFIRMACAO: Mensagem não enviada para o  agendamento id: " + agendamento.getId());
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        //TODO transformar em log para salvar em tb to banco
+        System.out.println("MENSAGEM CONFIRMACAO: RODOU O ENVIO");
     }
 
 }
