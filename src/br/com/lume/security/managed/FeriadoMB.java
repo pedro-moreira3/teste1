e0faa2a9507223e48f23d25c512c2421f99e6c31
Claudio Wolszczak <claudio.wolszczak@lumetec.com.br>
2021-09-01 15:59:16 -0300

    ajustes projeto

---
+++ src/br/com/lume/security/managed/FeriadoMB.java
@@ -0,0 +1,34 @@
+package br.com.lume.security.managed;
+
+
+import javax.annotation.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+import org.apache.log4j.Logger;
+
+import br.com.lume.common.managed.LumeManagedBean;
+//import br.com.lume.feriado.FeriadoSingleton;
+//import br.com.lume.security.entity.Feriado;
+
+@ManagedBean
+@ViewScoped
+public class FeriadoMB  {
+
+    private static final long serialVersionUID = 1L;
+
+//    private Logger log = Logger.getLogger(FeriadoMB.class);
+//
+//    public FeriadoMB() {
+//        super(FeriadoSingleton.getInstance().getBo());
+//        this.setClazz(Feriado.class);
+//        carregarTela();
+//    }
+//
+//    private void carregarTela() {
+//    }
+    
+
+    
+
+
+}
