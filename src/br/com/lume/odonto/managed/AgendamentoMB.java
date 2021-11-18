bc2335861d4b59dac766c899a81ef0ac5bfe925a
eduardo.tremarin <eduardo.tremarin@LUME159.mshome.net>
2021-11-18 17:20:16 -0300

    Correção visualização dos dados do paciente

--- src/br/com/lume/odonto/managed/AgendamentoMB.java
+++ src/br/com/lume/odonto/managed/AgendamentoMB.java
@@ -218,7 +218,7 @@ public class AgendamentoMB extends LumeManagedBean<Agendamento> {
 
         this.setClazz(Agendamento.class);
         try {
-            if (UtilsFrontEnd.getProfissionalLogado().getPerfil().equals(OdontoPerfil.DENTISTA) && UtilsFrontEnd.getEmpresaLogada().isEmpBolDentistaAdmin() == false && false) {
+            if (UtilsFrontEnd.getProfissionalLogado().getPerfil().equals(OdontoPerfil.DENTISTA) && UtilsFrontEnd.getEmpresaLogada().isEmpBolDentistaAdmin() == false) {
                 visivelDadosPaciente = false;
             }
             gmt = DominioSingleton.getInstance().getBo().findByEmpresaAndObjetoAndTipoAndValor("agendamento", "hora", "GM").getNome();
